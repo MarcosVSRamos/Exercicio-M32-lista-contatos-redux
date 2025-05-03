a023df12f8af2190b5839ec56f7dd94f75da4773
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2025-05-03 13:59:53 -0400

    ajustes pós remoção dos filtros

--- src/Containers/BarraDePesquisa/index.tsx
+++ src/Containers/BarraDePesquisa/index.tsx
@@ -1,17 +1,11 @@
-import FiltroCard from '../../components/FiltroCard'
 import { Campo } from '../../styles'
-import { Div, Filtros } from './styles'
+import { Div } from './styles'
 
 const BarraDePesquisa = () => {
   return (
     <aside>
       <Div>
         <Campo type="text" placeholder="Buscar Contato" />
-        <Filtros>
-          <FiltroCard />
-          <FiltroCard />
-          <FiltroCard />
-        </Filtros>
       </Div>
     </aside>
   )
--- src/Containers/BarraDePesquisa/styles.ts
+++ src/Containers/BarraDePesquisa/styles.ts
@@ -2,13 +2,6 @@ import styled from 'styled-components'
 
 export const Div = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding-top: 16px;
-`
-
-export const Filtros = styled.div`
-  display: flex;
   justify-content: center;
-  padding: 16px 0;
+  margin: 16px;
 `
